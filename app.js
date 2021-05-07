73b350c2da2f645d99f8d654194f23ae21ff790a
Chloeyayo <qq944823075@gmail.com>
2021-05-07 23:59:35 +0800

    完成

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const http = require('http');
 const url = require('url');
 const moment = require('moment');
-const List = [{
+const list = [{
   name: "John",
   content: "Lorem ipsum",
-  timestamp: '2021-5-7 23:11:18'
+  timestamp: '2021-05-07 23:57:50'
 }, {
   name: "John2",
   content: "Lorem ipsum2",
-  timestamp: '2021-5-7 23:12:18'
+  timestamp: '2021-05-07 23:57:50'
 }, {
   name: "John3",
   content: "Lorem ipsum3",
-  timestamp: '2021-5-7 23:10:18'
+  timestamp: '2021-05-07 23:57:50'
 }]
 http.createServer((req, res) => {
   let parsedUrl=url.parse(req.url,true)
   res.setHeader("Access-Control-Allow-Origin","http://127.0.0.1:5500");
   if (parsedUrl.pathname=="/post") {
-    console.log(JSON.stringify(parsedUrl.query));
     let query=parsedUrl.query
     query.timestamp=moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
-    console.log(query);
-    List.push(parsedUrl.query)
+    console.log(list);
+    list.push(parsedUrl.query)
     res.writeHead(200, {'Content-Type': 'application/json'});
-    return res.end(JSON.stringify(parsedUrl.query))
+    return res.end(JSON.stringify(list))
+  }else if(parsedUrl.pathname=="/get") {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(list))
   }
   res.end()
 
 }).listen(3000, ()=>{
-  console.log("Listening");
+  console.log("listening");
 })
