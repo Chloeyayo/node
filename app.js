a80622c53fb404ac604501300eaec28049637a63
Chloeyayo <qq944823075@gmail.com>
2021-05-14 12:36:08 +0800

    用express重构

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const http = require('http');
-const url = require('url');
-const moment = require('moment');
+const express= require('express');
+const app = express();
+const moment= require('moment');
 const list = [{
   name: "John",
   content: "Lorem ipsum",
-  timestamp: '2021-05-07 23:57:50'
+  timestamp: '2021-05-07 23:11:18'
 }, {
   name: "John2",
   content: "Lorem ipsum2",
-  timestamp: '2021-05-07 23:57:50'
+  timestamp: '2021-05-07 23:12:18'
 }, {
   name: "John3",
   content: "Lorem ipsum3",
-  timestamp: '2021-05-07 23:57:50'
+  timestamp: '2021-05-07 23:10:18'
 }]
-http.createServer((req, res) => {
-  let parsedUrl=url.parse(req.url,true)
+app.all("*",(_req, res,next) => {
   res.setHeader("Access-Control-Allow-Origin","http://127.0.0.1:5500");
-  if (parsedUrl.pathname=="/post") {
-    let query=parsedUrl.query
-    query.timestamp=moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
-    console.log(list);
-    list.push(parsedUrl.query)
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    return res.end(JSON.stringify(list))
-  }else if(parsedUrl.pathname=="/get") {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    return res.end(JSON.stringify(list))
-  }
-  res.end()
-
-}).listen(3000, ()=>{
-  console.log("listening");
+  next();
+})
+app.get('/post',(req, res)=>{
+  let query={}
+  query.name = req.query.name
+  query.content = req.query.content
+  query.timestamp =moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
+  list.push(query)
+  res.send(list)
+})
+app.get('/list',(req, res)=>{
+  res.send(list)
+})
+app.listen(3000,()=>{
+console.log('listening on 3000')
 })
